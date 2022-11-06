a9a9685e2150e3934f8629e5b86763290c3f180b
Kauan T. Eiras <43351064+kauaneiras@users.noreply.github.com>
2022-11-05 23:07:57 -0300

    Update app.js
    [sign-up] --> OK

--- app.js
+++ app.js
@@ -5,6 +5,15 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+let user = {username: "", avatar: ""};
+
+app.post('/sign-up', (req, res) => {
+    let login = req.body;
+    user.username = login.username;
+    user.avatar = login.avatar;
+    res.send("OK");  
+})
+
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
