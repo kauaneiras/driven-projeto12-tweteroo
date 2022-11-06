7239b98f3c1bd9f1597c7c6de128be48911807e8
Kauan T. Eiras <43351064+kauaneiras@users.noreply.github.com>
2022-11-05 23:49:42 -0300

    Update app.js
    [app] --> OK

--- app.js
+++ app.js
@@ -7,18 +7,42 @@ app.use(cors());
 
 let user = {username: "", avatar: ""};
 
+
 app.post('/sign-up', (req, res) => {
+    console.log("API Sign Up");
     let login = req.body;
-    user.username = login.username;
-    user.avatar = login.avatar;
+    user = {username: login.username, avatar: login.avatar}
     res.send("OK");  
 })
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-});
+let posts =[];
 
-const port = 5000;
-app.listen(port, () => {
-    console.log(`Running at http://localhost:${port}`)
+app.post('/tweets', (req, res) => {
+    console.log("API Tweets");
+    posts.push(
+        {
+            username: user.username,
+            avatar: user.avatar,
+            tweet: req.body.tweet,
+        }
+    );
+    res.send("OK");
+})
+
+app.get('/tweets', (req, res) => {
+    let alltweets = [];
+    if(posts.length > 10) {
+        for(let i = 0; i < 9; i++) {
+            alltweets.push(posts[posts.length -1 - i])
+        }
+    }else{
+        for(let i = 0; i < posts.length; i++) {
+            alltweets.push(posts[posts.length -1 - i])
+        }
+    }
+    res.send(alltweets)
+})
+
+app.listen(5000, () => {
+    console.log(`Running at http://localhost:${5000}`)
 })
