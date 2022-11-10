ed2c4ca479715e0f8f0f0be126882a9a6a8411af
Venyus Boldrini <venyustech@gmail.com>
2022-11-10 16:48:46 -0300

    refactor: code review - fine adjustments

--- app.js
+++ app.js
@@ -1,49 +1,49 @@
 import express from "express";
-import cors from 'cors';
+import cors from "cors";
 
 const app = express();
+
 app.use(express.json());
 app.use(cors());
 
-let user = {username: "", avatar: ""};
+let user = { username: "", avatar: "" };
+let posts = [];
 
+app.post("/sign-up", (req, res) => {
+  let login = req.body;
 
-app.post('/sign-up', (req, res) => {
-    console.log("API Sign Up");
-    let login = req.body;
-    user.username = login.username;
-    user.avatar = login.avatar;
-    res.send("OK");  
-})
+  user.username = login.username;
+  user.avatar = login.avatar;
 
-let posts =[];
+  res.send("OK");
+});
 
-app.post('/tweets', (req, res) => {
-    console.log("API Tweets");
-    posts.push(
-        {
-            username: user.username,
-            avatar: user.avatar,
-            tweet: req.body.tweet,
-        }
-    );
-    res.send("OK");
-})
+app.post("/tweets", (req, res) => {
+  posts.push({
+    username: user.username,
+    avatar: user.avatar,
+    tweet: req.body.tweet,
+  });
 
-app.get('/tweets', (req, res) => {
-    let alltweets = [];
-    if(posts.length > 10) {
-        for(let i = 0; i < 10; i++) {
-            alltweets.push(posts[posts.length -1 - i])
-        }
-    }else{
-        for(let i = 0; i < posts.length; i++) {
-            alltweets.push(posts[posts.length -1 - i])
-        }
+  res.send("OK");
+});
+
+app.get("/tweets", (req, res) => {
+  let alltweets = [];
+
+  if (posts.length > 10) {
+    for (let i = 0; i < 10; i++) {
+      alltweets.push(posts[posts.length - 1 - i]);
     }
-    res.send(alltweets)
-})
+  } else {
+    for (let i = 0; i < posts.length; i++) {
+      alltweets.push(posts[posts.length - 1 - i]);
+    }
+  }
+
+  res.send(alltweets);
+});
 
 app.listen(5000, () => {
-    console.log(`API is unning at http://localhost:5000`)
-})
+  console.log(`API is unning at http://localhost:5000`);
+});
